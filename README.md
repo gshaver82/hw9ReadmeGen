204356991d3dd229286e515668abda3b04e9c267
gshaver <geneshaver@gmail.com>
2020-05-30 22:10:01 -0500

    content filler
    added each section demanded by homework

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# bob is cool
+# title here
+# Descriptiondesc here
+# Table of ContentsTOC here
+# Installationinstall here
+# Usageusing here
+# Licenselic here
+# Contributingcontributing here
+# Teststesting here
+# Questionsquestions here
--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const axios = require("axios");
 
+// Title
+// Description
+// Table of Contents
+// Installation
+// Usage
+// License
+// Contributing
+// Tests
+// Questions
 
 
 inquirer
@@ -11,11 +20,62 @@ inquirer
             message: "Title of project?",
             name: "title"
         },
+        {
+            type: "input",
+            message: "Description?",
+            name: "Description"
+        },
+        {
+            type: "input",
+            message: "Table of Contents?",
+            name: "TableofContents"
+        },
+        {
+            type: "input",
+            message: "Installation?",
+            name: "Installation"
+        },
+        {
+            type: "input",
+            message: "Usage?",
+            name: "Usage"
+        },
+        {
+            type: "input",
+            message: "License?",
+            name: "License"
+        },
+        {
+            type: "input",
+            message: "Contributing?",
+            name: "Contributing"
+        },
+        {
+            type: "input",
+            message: "Tests?",
+            name: "Tests"
+        },
+        {
+            type: "input",
+            message: "Questions?",
+            name: "Questions"
+        },
+
 
     ]).then(function (data) {
 
         let stream = fs.createWriteStream("README.md");
-        stream.write("# " + data.title);
+        stream.write("# " + data.title + "\n");
+        stream.write("# Description" + data.Description + "\n");
+        stream.write("# Table of Contents" + data.TableofContents + "\n");
+        stream.write("# Installation" + data.Installation + "\n");
+        stream.write("# Usage" + data.Usage + "\n");
+        stream.write("# License" + data.License + "\n");
+        stream.write("# Contributing" + data.Contributing + "\n");
+        stream.write("# Tests" + data.Tests + "\n");
+        stream.write("# Questions" + data.Questions + "\n");
+        
+
     }).catch(function (error) {
         console.log("An error occured:", error);
     });
