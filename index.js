57e998a59615e23c6ae825cb46337811096d0728
gshaver <geneshaver@gmail.com>
2021-05-06 18:31:58 -0500

    updated generator

--- index.js
+++ index.js
@@ -2,23 +2,13 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const axios = require("axios");
 
-// Title
-// Description
-// Table of Contents
-// Installation
-// Usage
-// License
-// Contributing
-// Tests
-// Questions
-
-
 inquirer
     .prompt([
         {
             type: "input",
             message: "Github username?",
-            name: "userName"
+            name: "userName",
+            default: "gshaver82"
         },
         {
             type: "input",
@@ -27,26 +17,20 @@ inquirer
         },
         {
             type: "input",
+            message: "link to deployed app",
+            name: "Link",
+            default: "XXtempXX"
+        },
+        {
+            type: "input",
+            message: "Screenshot?",
+            name: "Screenshot",
+            default: "XXtempXX"
+        },
+        {
+            type: "input",
             message: "Description?",
             name: "Description"
-        },
-        {
-            type: "input",
-            message: "Table of Contents?",
-            name: "TableofContents",
-            default: "none"
-        },
-        {
-            type: "input",
-            message: "Installation?",
-            name: "Installation",
-            default: "npm install"
-        },
-        {
-            type: "input",
-            message: "Usage?",
-            name: "Usage",
-            default: "node index.js"
         },
         {
             type: "list",
@@ -65,49 +49,29 @@ inquirer
             name: "Contributing",
             default: "none"
         },
-        {
-            type: "input",
-            message: "Tests?",
-            name: "Tests",
-            default: "none"
-        },
-        // {
-        //     type: "input",
-        //     message: "Questions?",
-        //     name: "Questions"
-        // },
-
     ]).then(function (data) {
         let stream = fs.createWriteStream("./output/README.md");
         stream.write("# " + data.title + "\n");
+        stream.write("# link to deployed app" + "\n" + "<a href='" + data.Link +  "'>gshaver82</a>" + "\n\n");
+        stream.write("# Screenshot" + "\n" + "<img src='" + data.Screenshot +  "' alt=Screenshot>" + "\n\n");
         stream.write("# Description" + "\n" + data.Description + "\n");
-        stream.write("# Table of Contents" + "\n" + data.TableofContents + "\n");
-        stream.write("# Installation" + "\n" + data.Installation + "\n");
-        stream.write("# Usage" + "\n" + data.Usage + "\n");
         stream.write("# License" + "\n");
-
         if (data.License === "MIT") {
             stream.write("<a href='https://opensource.org/licenses/MIT'>" +
-                "<img alt='Badge' src='https://img.shields.io/badge/License-MIT-yellow.svg'>\n\n");
+                "<img alt='Badge' src='https://img.shields.io/badge/License-MIT-yellow.svg'></a>\n\n");
         } else if (data.License === "GNU GPL v3") {
             stream.write("<a href='https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html'>" +
-                "<img alt='Badge' src='https://img.shields.io/badge/License-GPLv3-blue.svg'>\n\n");
+                "<img alt='Badge' src='https://img.shields.io/badge/License-GPLv3-blue.svg'></a>\n\n");
         } else if (data.License === "BSD 3") {
             stream.write("<a href='https://opensource.org/licenses/BSD-3-Clause'>" +
-                "<img alt='Badge' src='https://img.shields.io/badge/License-BSD%203--Clause-blue.svg'>\n\n");
+                "<img alt='Badge' src='https://img.shields.io/badge/License-BSD%203--Clause-blue.svg'></a>\n\n");
         } else if (data.License === "Do what the F you want.") {
             stream.write("<a href='http://www.wtfpl.net/about/'>" +
-                "<img alt='Badge' src='https://img.shields.io/badge/License-WTFPL-brightgreen.svg'>\n\n");
+                "<img alt='Badge' src='https://img.shields.io/badge/License-WTFPL-brightgreen.svg'></a>\n\n");
         } else {
             stream.write("# License" + "\n" + data.License + "\n");
         }
-
-
-
-
         stream.write("# Contributing" + "\n" + data.Contributing + "\n");
-        stream.write("# Tests" + "\n" + data.Tests + "\n");
-        // stream.write("# Questions" + "\n" + data.Questions + "\n");
 
         const queryUrl = `https://api.github.com/users/${data.userName}`;
         axios
--- output/README.md
+++ output/README.md
@@ -1,21 +1,19 @@
-# HW10EmployeeSummaryTemplate
+# 
+# link to deployed app
+<a href='XXtempXX'>gshaver82</a>
+
+# Screenshot
+<img src='XXtempXX' alt=Screenshot>
+
 # Description
-This program will ask for managers interns and engineers and display them all on one webpage
-# Table of Contents
-none
-# Installation
-npm install
-# Usage
-node app.js
+
 # License
-<a href='http://www.wtfpl.net/about/'><img alt='Badge' src='https://img.shields.io/badge/License-WTFPL-brightgreen.svg'>
+<a href='https://opensource.org/licenses/MIT'><img alt='Badge' src='https://img.shields.io/badge/License-MIT-yellow.svg'></a>
 
 # Contributing
-Chue Yang
-# Tests
-node test to test for the classes getting the right info
+none
 # Questions
  Questions may be forwarded to me at my Github profile
 <a href='https://github.com/gshaver82'>gshaver82</a>
 
-<img src='https://avatars3.githubusercontent.com/u/52022933?v=4' alt=Github profile picture width=100>
+<img src='https://avatars.githubusercontent.com/u/52022933?v=4' alt=Github profile picture width=100>
